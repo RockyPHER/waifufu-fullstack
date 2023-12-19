a8051dac87e192338def7f7e03dd41a4261d3027
RockyPHER <davidsonjunior10@gmail.com>
2023-12-18 21:36:15 -0300

    trying to create slider

--- frontend/src/components/Carousel/Carousel.tsx
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -4,32 +4,42 @@ interface CarouselProps {
     waifus: Waifu[];
 }
 
+interface WaifuProfileProps {
+    waifu: Waifu;
+}
+
 export function Carousel({ waifus }: CarouselProps) {
 
-    // const [activeWaifuIdx, setActiveWaifuIdx] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
 
     function next() {
-
     }
     function previous() {
     }
 
     return (
-        <div className="relative h-screen flex items-center">
+        <div className="h-screen flex items-center">
             <button className="fixed left-0 bg-gray-300 text-black m-2 p-2" onClick={next}>next</button>
             <button className="fixed right-0 bg-gray-300 text-black m-2 p-2" onClick={previous}>previous</button>
-            <div className="flex flex-row w-full h-full">
+            <div className="absolute left-[] flex flex-row w-fit h-full">
                 {
                     waifus.map((waifu) => (
-                        <div key={waifu.id} className="w-full h-full">
-                            <div className="bg-cover w-full h-full"
-                                style={{ backgroundImage: `url(${waifu.imageUrl})` }} />
-                        </div>
+                        <WaifuProfile waifu={waifu} />
                     ))
                 }
-
             </div>
         </div>
     )
 
+}
+
+export function WaifuProfile({ waifu }: WaifuProfileProps) {
+
+    return (
+        <div className="w-screen h-screen bg-cover" key={waifu.id} style={{ backgroundImage: `url(${waifu.imageUrl})` }}>
+            <div className="bg-gradient-to-r from-gray-800 to-transparent w-1/2 h-1/3">
+
+            </div>
+        </div>
+    )
 }
