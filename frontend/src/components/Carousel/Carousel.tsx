1e37ef8989b675dddd52c727dc9b27cece354eb8
RockyPHER <davidsonjunior10@gmail.com>
2023-12-20 01:15:48 -0300

    added slider transition

--- frontend/src/components/Carousel/Carousel.tsx
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Waifu from "../../api/waifus/WaifusApiModel";
 
 interface CarouselProps {
@@ -10,18 +11,21 @@ interface WaifuProfileProps {
 
 export function Carousel({ waifus }: CarouselProps) {
 
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentWaifuIdx, setCurrentWaifuIdx] = useState(0);
 
     function next() {
+        setCurrentWaifuIdx((prev: number) => (prev + 1) % waifus.length)
     }
     function previous() {
+        setCurrentWaifuIdx((prev: number) => (prev - 1 + waifus.length) % waifus.length)
     }
 
     return (
         <div className="h-screen flex items-center">
-            <button className="fixed left-0 bg-gray-300 text-black m-2 p-2" onClick={next}>next</button>
-            <button className="fixed right-0 bg-gray-300 text-black m-2 p-2" onClick={previous}>previous</button>
-            <div className="absolute left-[] flex flex-row w-fit h-full">
+            <button className="fixed z-10 left-0 bg-gray-300 text-black m-2 p-2" onClick={previous}>previous</button>
+            <button className="fixed z-10 right-0 bg-gray-300 text-black m-2 p-2" onClick={next}>next</button>
+            <div className="absolute transition-transform duration-300 ease-in-out left-0 flex flex-row w-fit h-full"
+                style={{ transform: `translateX(-${currentWaifuIdx * 100}vw)` }}>
                 {
                     waifus.map((waifu) => (
                         <WaifuProfile waifu={waifu} />
@@ -37,8 +41,12 @@ export function WaifuProfile({ waifu }: WaifuProfileProps) {
 
     return (
         <div className="w-screen h-screen bg-cover" key={waifu.id} style={{ backgroundImage: `url(${waifu.imageUrl})` }}>
-            <div className="bg-gradient-to-r from-gray-800 to-transparent w-1/2 h-1/3">
-
+            <div className="absolute p-10 bottom-0 translate-y-[-35%] bg-gradient-to-r from-black to-transparent w-[50vw] h-[40vh]">
+                <h1 className="text-4xl text-white font-bold">{waifu.name}</h1>
+                <p className="text-white">{waifu.age}</p>
+                <p className="text-white">{waifu.height}</p>
+                <p className="text-white">{waifu.weight}</p>
+                <p className="text-white">{waifu.bio}</p>
             </div>
         </div>
     )
