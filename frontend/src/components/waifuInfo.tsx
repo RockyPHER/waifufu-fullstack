9af766d71c7835d62dd77c54bef0e184f8fd25fc
RockyPHER <davidsonjunior10@gmail.com>
2024-02-29 14:55:45 -0300

    <frontend> feat: waifuinfo intercaleted animation add

--- frontend/src/components/waifuInfo.tsx
+++ frontend/src/components/waifuInfo.tsx
@@ -8,7 +8,13 @@ import {
   Weight,
 } from "lucide-react";
 import Waifu from "../api/waifus/model";
-import { useEffect, useState, useTransition } from "react";
+import {
+  animated,
+  useSpring,
+  useSpringValue,
+  useSprings,
+} from "@react-spring/web";
+import { useEffect } from "react";
 
 interface WaifuInfoProps {
   waifu: Waifu;
@@ -16,76 +22,150 @@ interface WaifuInfoProps {
 
 export default function WaifuInfo({ waifu }: WaifuInfoProps) {
   const keyid = waifu.id;
-  const [isVisible, setIsVisible] = useState(false);
   const calculateDelay = (index: number) => {
-    return `${index * 50}ms`; // Adjust the delay as needed
+    return index * 10; // Adjust the delay as needed
   };
 
-  const transition = useTransition(isVisible, {
-    from: {},
-    enter: {},
-    leave: {},
+  const [props0, api0] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props1, api1] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props2, api2] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props3, api3] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props4, api4] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props5, api5] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props6, api6] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props7, api7] = useSpring(() => ({
+    from: { x: -200, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+  }));
+  const [props8, api8] = useSpring(() => ({}));
+
+  useEffect(() => {
+    api1.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 0,
+    });
+    api2.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 10,
+    });
+    api3.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 20,
+    });
+    api4.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 30,
+    });
+    api5.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 40,
+    });
+    api6.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 50,
+    });
+    api7.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 60,
+    });
+    api8.start({
+      from: { x: -200, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+      delay: 70,
+    });
+    api0.start({
+      from: { x: -100, opacity: 0 },
+      to: { x: 0, opacity: 1 },
+    });
   });
 
   return (
     <div className="w-1/2 h-auto absolute top-1/2 translate-y-[-20%] z-20 left-0 select-none bg-opacity-30">
-      <div className="w-full h-full absolute top-0 left-0 -z-10 animate-onload-left-8 bg-gradient-to-r from-black to-transparent"></div>
-      <span key={keyid} className={`flex flex-col gap-1 px-10 py-4`}>
-        <h1
-          className={`mb-4 font-bold text-4xl animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(1) }}
-        >
+      <animated.div
+        style={props0}
+        className="w-full h-full absolute top-0 left-0 -z-10 animate-onload-left-8 bg-gradient-to-r from-black to-transparent"
+      ></animated.div>
+      <span className={`flex flex-col gap-1 px-10 py-4`}>
+        <animated.h1 className={`mb-4 font-bold text-4xl`} style={props1}>
           {waifu.name}
-        </h1>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(2) }}
+        </animated.h1>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props2}
         >
           <Hourglass />
           {waifu.age} years
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(3) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props3}
         >
           <Globe2 />
           {waifu.origin}
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(4) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props4}
         >
           <Palette />
           {waifu.hairColor}
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(5) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props5}
         >
           <Eye />
           {waifu.eyeColor}
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(6) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props6}
         >
           <Ruler />
           {waifu.height}cm
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(7) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props7}
         >
           <Weight />
           {waifu.weight}kg
-        </p>
-        <p
-          className={`flex items-center text-lg font-semibold gap-3 animate-onload-left-3`}
-          style={{ animationDelay: calculateDelay(8) }}
+        </animated.p>
+        <animated.p
+          className={`flex items-center text-lg font-semibold gap-3`}
+          style={props8}
         >
           <CalendarFold />
           {waifu.birthday}
-        </p>
+        </animated.p>
       </span>
     </div>
   );
