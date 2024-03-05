7d5f55c9c243cbcb571d55598f47977cc112f747
RockyPHER <davidsonjunior10@gmail.com>
2024-03-05 20:58:44 -0300

    <frontend> style: color themes

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,6 +3,31 @@ import MenuButton from "../components/menuButton";
 import { useState } from "react";
 
 export default function Home() {
+  const colorTheme = {
+    darkMain: "rgba(0, 0, 0, 1)",
+    darkMid: "rgba(0, 0, 0, 0.6)",
+    darkBack: "rgba(0, 0, 0, 0.1)",
+    lightMain: "rgba(255, 255, 255, 1)",
+    lightMid: "rgba(255, 255, 255, 0.6)",
+    lightBack: "rgba(255, 255, 255, 0.1)",
+  };
+  const [darkMode, setDarkMode] = useState(false);
+  const [mainColor, setMainColor] = useState(colorTheme.lightMain);
+  const [midColor, setMidColor] = useState(colorTheme.lightMid);
+  const [backColor, setBackColor] = useState(colorTheme.lightBack);
+
+  const handleThemeChange = () => {
+    if (darkMode) {
+      setMainColor(colorTheme.darkMain);
+      setMidColor(colorTheme.darkMid);
+      setBackColor(colorTheme.darkBack);
+    } else {
+      setMainColor(colorTheme.lightMain);
+      setMidColor(colorTheme.lightMid);
+      setBackColor(colorTheme.lightBack);
+    }
+  };
+
   const backgrounds = [
     "https://images2.alphacoders.com/133/1330236.png",
     "https://images5.alphacoders.com/133/1338079.png",
@@ -12,50 +37,77 @@ export default function Home() {
     "https://images3.alphacoders.com/133/1339873.png",
   ];
 
+  const buttonThemes = {
+    dark: {
+      red: 0,
+      green: 0,
+      blue: 0,
+    },
+    light: {
+      red: 255,
+      green: 255,
+      blue: 255,
+    },
+  };
   const buttonConfigs = {
-    red: 255,
-    green: 255,
-    blue: 255,
+    ...buttonThemes[darkMode ? "dark" : "light"],
     alpha: 1,
     size: 6,
   };
-  const [darkMode, setDarkMode] = useState(false);
 
-  function handleClick() {
+  function handleTheme() {
     setDarkMode(!darkMode);
+    handleThemeChange();
   }
+  function handleStart() {}
   return (
-    <div className="w-full h-full relative bg-white">
+    <div className="w-full h-full relative">
       <section
         className="w-full h-full absolute flex justify-start pt-20 items-start bg-cover"
         style={{
           backgroundImage: `url(${backgrounds[4]})`,
         }}
       >
-        <button className="w-full h-auto absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-center group">
+        <button
+          onClick={handleStart}
+          className="w-full h-auto absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-center group"
+        >
           <ChevronDown
             size={128}
-            className="group-hover:animate-bounce transition-all text-white"
+            className={`group-hover:animate-bounce transition-all`}
+            style={{
+              color: `${midColor}`,
+            }}
           />
         </button>
       </section>
-      <nav className="w-full h-20 absolute top-0 flex justify-evenly items-center bg-transparent border-b-2 border-white border-opacity-10 shadow-lg backdrop-blur-sm">
+      <nav
+        className={`w-full h-20 absolute top-0 flex justify-evenly items-center border-b-2 shadow-lg backdrop-blur-sm`}
+        style={{
+          borderColor: `${midColor}`,
+          backgroundColor: `${backColor}`,
+        }}
+      >
         <MenuButton {...buttonConfigs} />
         <h1 className="select-none text-5xl font-bold capitalize animate-onload-up">
           waifufu
         </h1>
         <button
-          className="w-[64px] h-[32px] relative bg-white bg-opacity-20 rounded-full"
-          onClick={() => handleClick()}
+          className={`w-[64px] h-[32px] relative rounded-full`}
+          style={{ backgroundColor: `${mainColor}` }}
+          onClick={() => handleTheme()}
         >
           <div
-            className="absolute -top-[2px] right-0 flex items-center rounded-full border-2 border-white border-opacity-50 transition-all"
-            style={{ transform: `translateX(${darkMode ? -32 : 0}px)` }}
+            className={`absolute -top-[2px] right-0 flex items-center rounded-full border-2 transition-all`}
+            style={{
+              transform: `translateX(${darkMode ? -32 : 0}px)`,
+              borderColor: `${backColor}`,
+            }}
           >
             {darkMode ? (
-              <Moon size={32} className="text-white" />
+              <Moon size={32} style={{ stroke: `${mainColor}` }} />
             ) : (
-              <Sun size={32} className="text-white" />
+              <Sun size={32} style={{ stroke: `${mainColor}` }} />
             )}
           </div>
         </button>
