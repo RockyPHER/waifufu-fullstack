8d714d376c93a1cdba67a985599c676e248bf0c2
RockyPHER <davidsonjunior10@gmail.com>
2024-03-05 21:26:18 -0300

    <frontend> style: change theme button complete

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 export default function Home() {
   const colorTheme = {
     darkMain: "rgba(0, 0, 0, 1)",
-    darkMid: "rgba(0, 0, 0, 0.6)",
+    darkMid: "rgba(0, 0, 0, 0.5)",
     darkBack: "rgba(0, 0, 0, 0.1)",
     lightMain: "rgba(255, 255, 255, 1)",
-    lightMid: "rgba(255, 255, 255, 0.6)",
+    lightMid: "rgba(255, 255, 255, 0.3)",
     lightBack: "rgba(255, 255, 255, 0.1)",
   };
   const [darkMode, setDarkMode] = useState(false);
@@ -16,8 +16,8 @@ export default function Home() {
   const [midColor, setMidColor] = useState(colorTheme.lightMid);
   const [backColor, setBackColor] = useState(colorTheme.lightBack);
 
-  const handleThemeChange = () => {
-    if (darkMode) {
+  const handleThemeChange = (bool: boolean) => {
+    if (bool === true) {
       setMainColor(colorTheme.darkMain);
       setMidColor(colorTheme.darkMid);
       setBackColor(colorTheme.darkBack);
@@ -56,8 +56,10 @@ export default function Home() {
   };
 
   function handleTheme() {
-    setDarkMode(!darkMode);
-    handleThemeChange();
+    setDarkMode((prev) => {
+      handleThemeChange(!prev);
+      return !prev;
+    });
   }
   function handleStart() {}
   return (
@@ -66,6 +68,7 @@ export default function Home() {
         className="w-full h-full absolute flex justify-start pt-20 items-start bg-cover"
         style={{
           backgroundImage: `url(${backgrounds[4]})`,
+          transition: "all 0.3s ease-in-out",
         }}
       >
         <button
@@ -76,7 +79,8 @@ export default function Home() {
             size={128}
             className={`group-hover:animate-bounce transition-all`}
             style={{
-              color: `${midColor}`,
+              color: `${mainColor}`,
+              transition: "all 0.3s ease-in-out",
             }}
           />
         </button>
@@ -84,30 +88,56 @@ export default function Home() {
       <nav
         className={`w-full h-20 absolute top-0 flex justify-evenly items-center border-b-2 shadow-lg backdrop-blur-sm`}
         style={{
-          borderColor: `${midColor}`,
+          borderColor: `${backColor}`,
           backgroundColor: `${backColor}`,
+          transition: "all 0.3s ease-in-out",
         }}
       >
         <MenuButton {...buttonConfigs} />
-        <h1 className="select-none text-5xl font-bold capitalize animate-onload-up">
+        <h1
+          className="select-none text-5xl font-bold capitalize animate-onload-up"
+          style={{
+            color: `${mainColor}`,
+            transition: "all 0.3s ease-in-out",
+          }}
+        >
           waifufu
         </h1>
         <button
           className={`w-[64px] h-[32px] relative rounded-full`}
-          style={{ backgroundColor: `${mainColor}` }}
+          style={{ backgroundColor: `${midColor}` }}
           onClick={() => handleTheme()}
         >
           <div
-            className={`absolute -top-[2px] right-0 flex items-center rounded-full border-2 transition-all`}
+            className={`absolute -top-[2px] right-0 flex items-center rounded-full border-2`}
             style={{
               transform: `translateX(${darkMode ? -32 : 0}px)`,
-              borderColor: `${backColor}`,
+              backgroundColor: `${
+                darkMode ? colorTheme.lightBack : colorTheme.darkBack
+              }`,
+              borderColor: `${
+                darkMode ? colorTheme.darkMain : colorTheme.lightMid
+              }`,
+              transition: "all 0.3s ease-in-out",
             }}
           >
             {darkMode ? (
-              <Moon size={32} style={{ stroke: `${mainColor}` }} />
+              <Moon
+                size={32}
+                style={{
+                  stroke: `${midColor}`,
+                  fill: `${mainColor}`,
+                  transition: "all 0.3s ease-in-out",
+                }}
+              />
             ) : (
-              <Sun size={32} style={{ stroke: `${mainColor}` }} />
+              <Sun
+                size={32}
+                style={{
+                  stroke: `${mainColor}`,
+                  transition: "all 0.3s ease-in-out",
+                }}
+              />
             )}
           </div>
         </button>
